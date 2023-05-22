4374bdd85a8748b910760dcfe681d102ae9b9a30
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-05-22 18:16:58 -0300

    att 22/05 tarde

---
+++ Conteudo/49_ketdown_keyup/script.js
@@ -0,0 +1,20 @@
+// keydown
+
+document.addEventListener("keydown", function(event) {
+
+    if(event.key === "Enter") {
+        console.log("Apertou enter");
+    };
+
+});
+
+// keyup
+
+document.addEventListener("keyup", function(ev) {
+
+    if(ev.key === "Enter") {
+        console.log("Soltou enter");
+    }
+
+
+});
